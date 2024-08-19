0c7531eb9e415d8ee630c32ef5d8145858ba9494
Spideyofficial777 <gaminghatyar777@gmail.com>
2024-08-19 12:29:56 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,6 +1,6 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit @SPIDEY_OFFICIAL_777
+# Subscribe YouTube Channel For Amazing Bot @SPIDEY_OFFICIAL_777
+# Ask Doubt on telegram @SPIDEY_OFFICIAL_777
 
 
 from os import path, getenv
