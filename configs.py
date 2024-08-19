53a9366613655d2630ebc1ab6771bb2883231de5
Spideyofficial777 <gaminghatyar777@gmail.com>
2024-08-19 12:28:29 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,16 +6,15 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "20389440"))
-    API_HASH = getenv("API_HASH", "a1a06a18eb9153e9dbd447cfd5da2457")
-    BOT_TOKEN = getenv("BOT_TOKEN", "6564513574:AAGDqUaEmeu0m4DjLDetNc4nooVTWYT7Fzo")
-    FSUB = getenv("FSUB", "VJ_Botz")
-    CHID = int(getenv("CHID", "-1001623633000"))
-    SUDO = list(map(int, getenv("SUDO", "6168162777").split()))
-    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://sushankm16:4i1WAfPYKWyqPIDD@cluster0.sngp9pz.mongodb.net/?retryWrites=true&w=majority")
+    API_ID = int(getenv("API_ID", "28519661"))
+    API_HASH = getenv("API_HASH", "d47c74c8a596fd3048955b322304109d")
+    BOT_TOKEN = getenv("BOT_TOKEN", "7236731343:AAHzJYfnwvBQDekYq8sSKY5cXS1GgJTQsgk")
+    FSUB = getenv("FSUB", "SPIDEYOFFICIAL777")
+    CHID = int(getenv("CHID", " -1001959922658"))
+    SUDO = list(map(int, getenv("SUDO", "5518489725").split()))
     
 cfg = Config()
 
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit @SPIDEY_OFFICIAL_777
+# Subscribe YouTube Channel For Amazing Bot @SPIDEY_OFFICIAL_777
+# Ask Doubt on telegram @SPIDEY_OFFICIAL_777
